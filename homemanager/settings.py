2bc6f6f096aa6fc89441b37ffcf3ef2788dc66bf
Md. Delwar Shahadat Deepu <63228841+deepu1067@users.noreply.github.com>
2024-10-04 00:09:51 +0600

    Update settings.py

--- homemanager/settings.py
+++ homemanager/settings.py
@@ -8,12 +8,6 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-ii+yi9tw846ppw+)gf#p@xzsvkeyq+86kxa7tuy#$4v&xaf^qk'
-
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
